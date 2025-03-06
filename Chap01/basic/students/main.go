909bad91de0c8a0701718ebf272d529b453c2655
Mauricio Rojas <mrojasb2000@gmail.com>
2025-03-06 18:34:10 -0300

    chore: add students ID's to collection

--- Chap01/basic/students/main.go
+++ Chap01/basic/students/main.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+// The function addStudent takes a slice of string representing the current collection of
+// students as the first parameter and a string representing a new student to be added to
+// the collection as the second parameter.
 func addStudent(students []string, student string) []string {
+	return append(students, student)
+}
+
+// The function addStudentID takes a slice of int representing the current collection of
+// students ID's as the first parameter and a int representing a new student ID to be added to
+// the collection as the second parameter.
+func addStudentID(students []int, student int) []int {
 	return append(students, student)
 }
 
@@ -12,4 +22,10 @@ func main() {
 	result = addStudent(result, "Jennifer")
 	result = addStudent(result, "Elaine")
 	fmt.Println(result)
+
+	students1 := []int{}
+	result1 := addStudentID(students1, 155)
+	result1 = addStudentID(result1, 112)
+	result1 = addStudentID(result1, 120)
+	fmt.Println(result1)
 }
