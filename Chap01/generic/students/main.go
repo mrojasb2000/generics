f82e60f5b587b2f2a1f064b25d2f6395cd3742d7
Mauricio Rojas <mrojasb2000@gmail.com>
2025-03-06 19:14:14 -0300

    chore: add data testing

--- Chap01/generic/students/main.go
+++ Chap01/generic/students/main.go
@@ -41,4 +41,17 @@ func main() {
 	result = addStudent(result, "Jennifer")
 	result = addStudent(result, "Elaine")
 	fmt.Println(result)
+
+	students1 := []Integer{}
+	result1 := addStudent(students1, 45)
+	result1 = addStudent(result1, 64)
+	result1 = addStudent(result1, 78)
+	fmt.Println(result1)
+
+	students2 := []Student{}
+	result2 := addStudent(students2, Student{Name: "John", ID: 213, Age: 17.5})
+	result2 = addStudent(result2, Student{Name: "James", ID: 111, Age: 18.75})
+	result2 = addStudent(result2, Student{Name: "Marsha", ID: 110, Age: 16.25})
+	fmt.Println(result2)
+
 }
