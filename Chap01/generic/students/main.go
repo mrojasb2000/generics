005a629f163177bc422127dd6628ad4e473dceca
Mauricio Rojas <mrojasb2000@gmail.com>
2025-03-06 19:30:12 -0300

    docs: refactor comments

--- Chap01/generic/students/main.go
+++ Chap01/generic/students/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// data type restricted by Stringer interfaces
+// we constrain the data type using Stringer interfaces
 type Stringer = interface {
 	String() string
 }
